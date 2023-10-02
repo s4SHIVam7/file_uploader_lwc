f03316650c08d2419c17000bf65096227c24e10c
Shivam Laidwar <60181328+s4SHIVam7@users.noreply.github.com>
2023-10-02 22:31:04 +0530

    Js ile updated for File Uplaoder LWC (#3)
    * js file updated.

--- force-app/main/default/lwc/fileUploaderLwc/fileUploaderLwc.js
+++ force-app/main/default/lwc/fileUploaderLwc/fileUploaderLwc.js
@@ -1,4 +1,27 @@
-import { LightningElement } from 'lwc';
-export default class FileUploaderLwc extends LightningElement {
 
-}
+import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class FileUploaderLWC extends LightningElement {
+    @api recordId;
+
+    handleUploadFinished(event) {
+        const uploadFiles = event.detail.files;
+
+        let toastMessage = `${uploadFiles.length} Files uploaded Successfully:\n`;
+        for (let i = 0; i < uploadFiles.length; i++) {
+            const file = uploadFiles[i];
+            const docId = file.documentId;
+            const docUrl = `/lightning/r/ContentDocument/${docId}/view`;;
+             toastMessage += `${file.name} -> ${docId} : (${docUrl})\n`;
+        }
+      
+
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Success',
+                message: toastMessage,
+                variant: 'success',
+            })
+        );
+    }
